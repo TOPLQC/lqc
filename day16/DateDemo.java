02ee4c9d677b1f19ba2bf2e354af809f71677329
TOPLQC <1551997348@qq.com>
2023-08-28 14:54:07 +0800

    新的一天，从大建开始，乐，第16天，Data

---
+++ day16/DateDemo.java
@@ -0,0 +1,14 @@
+package com.wnxy.day16;
+
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String[] args) {
+        demo01();
+    }
+
+    public static void demo01() {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
