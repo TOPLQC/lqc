8199858a1268b73fe5045c0b081806265e8d1d45
TOPLQC <1551997348@qq.com>
2023-08-28 16:19:09 +0800

    new update

---
+++ utils/IdUtil.java
@@ -0,0 +1,18 @@
+package com.wnxy.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+public class IdUtil {
+    //工具类中的都是静态方法
+    public static String createId() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter yyyyMMddHHmmss = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String timeStr = now.format(yyyyMMddHHmmss);
+        Random random = new Random();
+        int number = random.nextInt(10000);
+        String id = String.format("%s%04d", timeStr, number);
+        return id;
+    }
+}
