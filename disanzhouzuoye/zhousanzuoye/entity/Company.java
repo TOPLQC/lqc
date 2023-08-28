bfd11e68d2d1b2685ac85214b701ee4f1710aa2f
TOPLQC <1551997348@qq.com>
2023-08-28 20:17:56 +0800

    new update

--- disanzhouzuoye/zhousanzuoye/entity/Company.java
+++ disanzhouzuoye/zhousanzuoye/entity/Company.java
@@ -1,11 +1,7 @@
 package com.wnxy.disanzhouzuoye.zhousanzuoye.entity;
 
 import com.wnxy.disanzhouzuoye.utils.IdUtil;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
 public class Company {
     private String id;
     private String name;
@@ -23,54 +19,51 @@ public class Company {
         this.boss = items[2];
         this.year = Integer.parseInt(items[3]);
     }
-//
-//    public Company(int id, String name, String boss, int year) {
-//        this.id = id;
-//        this.name = name;
-//        this.boss = boss;
-//        this.year = year;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Company{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", boss='" + boss + '\'' +
-//                ", year=" + year +
-//                '}';
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getBoss() {
-//        return boss;
-//    }
-//
-//    public void setBoss(String boss) {
-//        this.boss = boss;
-//    }
-//
-//    public int getYear() {
-//        return year;
-//    }
-//
-//    public void setYear(int year) {
-//        this.year = year;
-//    }
+
+    public Company(String id, String name, String boss, int year) {
+        this.id = id;
+        this.name = name;
+        this.boss = boss;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        String[] items = {this.id, this.name, this.boss, String.valueOf(this.year)};
+        String message = String.join("-", items);
+        return message;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBoss() {
+        return boss;
+    }
+
+    public void setBoss(String boss) {
+        this.boss = boss;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 
 }
