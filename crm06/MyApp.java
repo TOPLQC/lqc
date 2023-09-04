6830c90b10397ad2fd4cf575ac8c2e2c3c8af10b
TOPLQC <1551997348@qq.com>
2023-09-04 18:48:07 +0800

    new update

---
+++ crm06/MyApp.java
@@ -0,0 +1,10 @@
+package com.wnxy.crm06;
+
+import com.wnxy.crm06.view.MyView;
+
+public class MyApp {
+    public static void main(String[] args) {
+        MyView myView = new MyView();
+        myView.start();
+    }
+}
